886146211ba37760e540b33c65c20a1cbf090cc0
Troy Kelly <troy@aperim.com>
2021-09-19 17:21:09 +1000

    No more chown

--- mount.sh
+++ mount.sh
@@ -179,7 +179,7 @@ trap _term SIGTERM
 
 fusermount -u /mount${DRIVE_MOUNTFOLDER} || true
 mkdir -p /mount${DRIVE_MOUNTFOLDER} || true
-chown -R ${SHARE_UID}:${SHARE_GID} /mount${DRIVE_MOUNTFOLDER} || true
+# chown -R ${SHARE_UID}:${SHARE_GID} /mount${DRIVE_MOUNTFOLDER} || true
 ${RCLONE} lsd --config ${RCLONE_CONFIG} ${RCLONE_CRYPT_STORE}:${DRIVE_TARGETFOLDER}
 RCLONECMD="${RCLONE} mount --config ${RCLONE_CONFIG} --allow-non-empty --vfs-cache-mode ${RCLONE_ZFS_CACHE_MODE} --buffer-size ${RCLONE_BUFFER_SIZE} --vfs-read-ahead ${RCLONE_ZFS_READ_AHEAD} ${RCLONE_CRYPT_STORE}:${DRIVE_TARGETFOLDER} /mount${DRIVE_MOUNTFOLDER}"
 while :
